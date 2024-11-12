e95c8770a816618042e0b0886463162392bfec90
DaffaRahman11 <fauzirahman1105@gmail.com>
2024-11-12 23:04:23 +0700

    first commit

---
+++ public_html/lang/id/slider.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Slider Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'slider_1_title' => 'Bantuan kecil, Untuk sebab yang besar!',
+    'slider_1_subtitle' => 'Bantuan yang kecil dapat memberikan dampak yang besar',
+
+    'slider_2_title' => 'Bantu orang didekatmu',
+    'slider_2_subtitle' => 'Membantu orang disekitarmu agar masyarakat menjadi sejahtera',
+
+    'slider_3_title' => 'Mendidik untuk masa depan',
+    'slider_3_subtitle' => 'Dengan sebuah kontribusi kecil kamu bisa membantu ratusan anak yang membutuhkan pendidikan',
+
+
+
+
+
+];
